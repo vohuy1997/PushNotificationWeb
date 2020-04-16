6cefd0798ce9fea1f609ad88632539021bb98a42
vananboxyzvn <55480569+vananboxyzvn@users.noreply.github.com>
2020-04-16 09:15:27 +0700

    Revert "[ADD][FIX][DEL]Create layout (#1)"

--- PushNotificationWeb/resources/views/vendor/leantony/grid/pagination/pagination-links.blade.php
+++
@@ -1,5 +0,0 @@
-@if($grid->wantsPagination())
-    <div class="pull-right">
-        {{ $grid->getData()->appends(request()->query())->links($grid->getGridPaginationView(), ['pjaxTarget' => $grid->getId()]) }}
-    </div>
-@endif
